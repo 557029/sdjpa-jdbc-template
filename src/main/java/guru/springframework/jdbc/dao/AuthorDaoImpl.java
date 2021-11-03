9fa8ce5c7a9b86f1089cc9d569422dec99a2899f
Igor Aleshin <aleshin_igor@bah.com>
2021-11-03 16:09:09 -0400

    Added AuthorDao

---
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -0,0 +1,59 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Author;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthorDaoImpl implements AuthorDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public AuthorDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Author getById(Long id) {
+        return this.jdbcTemplate.queryForObject("SELECT * FROM author WHERE id = ?", getRowMapper(), id);
+    }
+
+    @Override
+    public Author getByName(String firstName, String lastName) {
+        return this.jdbcTemplate.queryForObject("SELECT * FROM author WHERE first_name = ? AND last_name = ?",
+                getRowMapper(),
+                firstName,
+                lastName);
+
+    }
+
+    @Override
+    public Author saveNewAuthor(Author author) {
+
+        this.jdbcTemplate.update("INSERT INTO author(first_name, last_name) VALUES (?, ?)",
+                author.getFirstName(), author.getLastName());
+        final Long id = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Long.class);
+        assert id != null;
+        return this.getById(id);
+    }
+
+    @Override
+    public Author updateAuthor(Author author) {
+        this.jdbcTemplate.update("UPDATE author SET first_name = ?, last_name = ? WHERE id = ?",
+                author.getFirstName(),
+                author.getLastName(),
+                author.getId()
+        );
+        return this.getById(author.getId());
+
+    }
+
+    @Override
+    public void deleteAuthorById(Long id) {
+        this.jdbcTemplate.update("DELETE FROM author WHERE id = ?", id);
+    }
+
+    private RowMapper<Author> getRowMapper() {
+        return new AuthorMapper();
+    }
+}
--- src/test/java/guru/springframework/jdbc/MySQLIntegrationTest.java
+++ src/test/java/guru/springframework/jdbc/MySQLIntegrationTest.java
@@ -1,28 +1,84 @@
 package guru.springframework.jdbc;
 
 
-import guru.springframework.jdbc.repositories.BookRepository;
+import guru.springframework.jdbc.dao.AuthorDao;
+import guru.springframework.jdbc.domain.Author;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ActiveProfiles("local")
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ComponentScan(basePackages = {"guru.springframework.jdbc.dao"})
 public class MySQLIntegrationTest {
 
     @Autowired
-    BookRepository bookRepository;
+    AuthorDao authorDao;
 
     @Test
-    void testMySQL() {
-        long countBefore = bookRepository.count();
-        assertThat(countBefore).isGreaterThan(0);
+    void testDeleteAuthor() {
+        Author author = new Author();
+        author.setFirstName("john");
+        author.setLastName("t");
+
+        Author saved = authorDao.saveNewAuthor(author);
+
+        authorDao.deleteAuthorById(saved.getId());
+
+        assertThrows(EmptyResultDataAccessException.class, () -> {
+            authorDao.getById(saved.getId());
+        });
     }
+
+    @Test
+    void testUpdateAuthor() {
+        Author author = new Author();
+        author.setFirstName("john");
+        author.setLastName("t");
+
+        Author saved = authorDao.saveNewAuthor(author);
+
+        saved.setLastName("Thompson");
+        Author updated = authorDao.updateAuthor(saved);
+
+        assertThat(updated.getLastName()).isEqualTo("Thompson");
+    }
+
+    @Test
+    void testInsertAuthor() {
+        Author author = new Author();
+        author.setFirstName("john");
+        author.setLastName("t");
+
+        Author saved = authorDao.saveNewAuthor(author);
+
+        assertThat(saved).isNotNull();
+    }
+
+    @Test
+    void testGetAuthorByName() {
+        Author author = authorDao.getByName("Craig", "Walls");
+
+        assertThat(author).isNotNull();
+    }
+
+    @Test
+    void testGetAuthor() {
+
+        Author author = authorDao.getById(1l);
+
+        assertThat(author.getId()).isNotNull();
+    }
+
+
 
 }
 
